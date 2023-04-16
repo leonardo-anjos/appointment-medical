acf9e5038daee51130ee50ff0fab04ad46759566
Leonardo Anjos <leodrums11@gmail.com>
2023-04-15 23:15:37 -0300

    feat: green step - throw exception

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -4,6 +4,10 @@ import { AppointmentInput } from './appointment.interface';
 @Injectable()
 export class AppointmentService {
   public scheduleAppointment(appointmentData: AppointmentInput) {
+    if (appointmentData.endTime < appointmentData.startTime) {
+      throw new Error(`appointment's endTime should be after startTime`);
+    }
+
     return {
       ...appointmentData,
       confirmed: false,
