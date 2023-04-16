104ac10a68e727d2a676d785e770eded7e36d972
Leonardo Anjos <leodrums11@gmail.com>
2023-04-16 02:19:16 -0300

    feat: green step - unit test scenario

--- src/patient/patient.service.spec.ts
+++ src/patient/patient.service.spec.ts
@@ -16,6 +16,10 @@ describe('PatientService', () => {
     expect(service).toBeDefined();
   });
 
+  // 1. The patientId should be validated?
+  // 2. We should be able to register a patient.
+  // 3. We should be able to say when a patient exists.
+
   describe('register', () => {
     it('should return a new patient with given name', async () => {
       const newPatient = await service.register({ name: 'John Doe' });
